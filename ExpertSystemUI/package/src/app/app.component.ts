999a5161a8fd7482e44c0ab1183396f9df227b45
Yumeng Zhou <yumengz@umich.edu>
2025-02-09 00:22:48 -0500

    .

--- ExpertSystemUI/package/src/app/app.component.ts
+++ ExpertSystemUI/package/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'MaterialPro Angular Admin Template';
+  title = '智筑专家';
 }
